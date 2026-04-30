2db279bbaefae0460cdab089a3e09727ea143918
Kris.Culin <kris.culin@bentley.com>
2026-04-30 08:09:07 -0400

    Implement Legend options editor for Phase 5
    - Update LegendEditorControl to provide legend visibility and placement editing
    - Add Show Legend toggle mapped exclusively to LegendPlacement.None
    - Add placement radio buttons for Top, Bottom, Left, and Right
    - Remember and restore last non-None placement when re-enabling legend
    - Disable placement controls when legend is hidden
    - Preserve model purity with no IsVisible flag or additional state
    - Limit changes strictly to LegendEditorControl with no cross-editor interactions
    - Verify full solution build and all existing tests pass cleanly

--- src/Graphing.Editors/Controls/LegendEditorControl.Designer.cs
+++ src/Graphing.Editors/Controls/LegendEditorControl.Designer.cs
@@ -16,7 +16,98 @@ namespace Graphing.Editors.Controls
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this._showLegendCheckBox = new System.Windows.Forms.CheckBox();
+            this._placementLabel = new System.Windows.Forms.Label();
+            this._topRadioButton = new System.Windows.Forms.RadioButton();
+            this._bottomRadioButton = new System.Windows.Forms.RadioButton();
+            this._leftRadioButton = new System.Windows.Forms.RadioButton();
+            this._rightRadioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // _showLegendCheckBox
+            // 
+            this._showLegendCheckBox.AutoSize = true;
+            this._showLegendCheckBox.Location = new System.Drawing.Point(12, 12);
+            this._showLegendCheckBox.Name = "_showLegendCheckBox";
+            this._showLegendCheckBox.Size = new System.Drawing.Size(92, 19);
+            this._showLegendCheckBox.TabIndex = 0;
+            this._showLegendCheckBox.Text = "Show Legend";
+            this._showLegendCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // _placementLabel
+            // 
+            this._placementLabel.AutoSize = true;
+            this._placementLabel.Location = new System.Drawing.Point(12, 42);
+            this._placementLabel.Name = "_placementLabel";
+            this._placementLabel.Size = new System.Drawing.Size(59, 15);
+            this._placementLabel.TabIndex = 1;
+            this._placementLabel.Text = "Placement";
+            // 
+            // _topRadioButton
+            // 
+            this._topRadioButton.AutoSize = true;
+            this._topRadioButton.Location = new System.Drawing.Point(12, 64);
+            this._topRadioButton.Name = "_topRadioButton";
+            this._topRadioButton.Size = new System.Drawing.Size(45, 19);
+            this._topRadioButton.TabIndex = 2;
+            this._topRadioButton.TabStop = true;
+            this._topRadioButton.Text = "Top";
+            this._topRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // _bottomRadioButton
+            // 
+            this._bottomRadioButton.AutoSize = true;
+            this._bottomRadioButton.Location = new System.Drawing.Point(12, 89);
+            this._bottomRadioButton.Name = "_bottomRadioButton";
+            this._bottomRadioButton.Size = new System.Drawing.Size(67, 19);
+            this._bottomRadioButton.TabIndex = 3;
+            this._bottomRadioButton.TabStop = true;
+            this._bottomRadioButton.Text = "Bottom";
+            this._bottomRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // _leftRadioButton
+            // 
+            this._leftRadioButton.AutoSize = true;
+            this._leftRadioButton.Location = new System.Drawing.Point(12, 114);
+            this._leftRadioButton.Name = "_leftRadioButton";
+            this._leftRadioButton.Size = new System.Drawing.Size(43, 19);
+            this._leftRadioButton.TabIndex = 4;
+            this._leftRadioButton.TabStop = true;
+            this._leftRadioButton.Text = "Left";
+            this._leftRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // _rightRadioButton
+            // 
+            this._rightRadioButton.AutoSize = true;
+            this._rightRadioButton.Location = new System.Drawing.Point(12, 139);
+            this._rightRadioButton.Name = "_rightRadioButton";
+            this._rightRadioButton.Size = new System.Drawing.Size(54, 19);
+            this._rightRadioButton.TabIndex = 5;
+            this._rightRadioButton.TabStop = true;
+            this._rightRadioButton.Text = "Right";
+            this._rightRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // LegendEditorControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._rightRadioButton);
+            this.Controls.Add(this._leftRadioButton);
+            this.Controls.Add(this._bottomRadioButton);
+            this.Controls.Add(this._topRadioButton);
+            this.Controls.Add(this._placementLabel);
+            this.Controls.Add(this._showLegendCheckBox);
+            this.Name = "LegendEditorControl";
+            this.Size = new System.Drawing.Size(546, 346);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.CheckBox _showLegendCheckBox;
+        private System.Windows.Forms.Label _placementLabel;
+        private System.Windows.Forms.RadioButton _topRadioButton;
+        private System.Windows.Forms.RadioButton _bottomRadioButton;
+        private System.Windows.Forms.RadioButton _leftRadioButton;
+        private System.Windows.Forms.RadioButton _rightRadioButton;
     }
 }
--- src/Graphing.Editors/Controls/LegendEditorControl.cs
+++ src/Graphing.Editors/Controls/LegendEditorControl.cs
@@ -1,3 +1,5 @@
+using System;
+using Graphing.Controls.Presentation;
 using System.Windows.Forms;
 using Graphing.Editors.EditorModels;
 
@@ -6,15 +8,131 @@ namespace Graphing.Editors.Controls
     public partial class LegendEditorControl : UserControl
     {
         private LegendEditorModel _model;
+        private bool _isUpdatingUi;
+        private LegendPlacement _lastNonNonePlacement;
 
         public LegendEditorControl()
         {
             InitializeComponent();
+            _lastNonNonePlacement = LegendPlacement.Bottom;
+            WireEvents();
+            UpdateUiFromModel();
         }
 
         public void LoadControl(LegendEditorModel model)
         {
             _model = model;
+
+            if (_model.Position != LegendPlacement.None)
+            {
+                _lastNonNonePlacement = _model.Position;
+            }
+
+            UpdateUiFromModel();
+        }
+
+        private void WireEvents()
+        {
+            _showLegendCheckBox.CheckedChanged += showLegendCheckBox_CheckedChanged;
+            _topRadioButton.CheckedChanged += placementRadioButton_CheckedChanged;
+            _bottomRadioButton.CheckedChanged += placementRadioButton_CheckedChanged;
+            _leftRadioButton.CheckedChanged += placementRadioButton_CheckedChanged;
+            _rightRadioButton.CheckedChanged += placementRadioButton_CheckedChanged;
+        }
+
+        private void showLegendCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_isUpdatingUi || _model == null)
+            {
+                return;
+            }
+
+            if (_showLegendCheckBox.Checked)
+            {
+                if (_lastNonNonePlacement == LegendPlacement.None)
+                {
+                    _lastNonNonePlacement = LegendPlacement.Bottom;
+                }
+
+                _model.Position = _lastNonNonePlacement;
+            }
+            else
+            {
+                if (_model.Position != LegendPlacement.None)
+                {
+                    _lastNonNonePlacement = _model.Position;
+                }
+
+                _model.Position = LegendPlacement.None;
+            }
+
+            UpdateUiFromModel();
+        }
+
+        private void placementRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_isUpdatingUi || _model == null)
+            {
+                return;
+            }
+
+            var button = sender as RadioButton;
+            if (button == null || !button.Checked || !_showLegendCheckBox.Checked)
+            {
+                return;
+            }
+
+            if (button == _topRadioButton)
+            {
+                _model.Position = LegendPlacement.Top;
+            }
+            else if (button == _bottomRadioButton)
+            {
+                _model.Position = LegendPlacement.Bottom;
+            }
+            else if (button == _leftRadioButton)
+            {
+                _model.Position = LegendPlacement.Left;
+            }
+            else if (button == _rightRadioButton)
+            {
+                _model.Position = LegendPlacement.Right;
+            }
+
+            if (_model.Position != LegendPlacement.None)
+            {
+                _lastNonNonePlacement = _model.Position;
+            }
+        }
+
+        private void UpdateUiFromModel()
+        {
+            _isUpdatingUi = true;
+            try
+            {
+                var position = _model != null ? _model.Position : LegendPlacement.None;
+                var showLegend = position != LegendPlacement.None;
+
+                _showLegendCheckBox.Checked = showLegend;
+
+                _topRadioButton.Enabled = showLegend;
+                _bottomRadioButton.Enabled = showLegend;
+                _leftRadioButton.Enabled = showLegend;
+                _rightRadioButton.Enabled = showLegend;
+
+                var selectedPlacement = showLegend
+                    ? position
+                    : (_lastNonNonePlacement == LegendPlacement.None ? LegendPlacement.Bottom : _lastNonNonePlacement);
+
+                _topRadioButton.Checked = selectedPlacement == LegendPlacement.Top;
+                _bottomRadioButton.Checked = selectedPlacement == LegendPlacement.Bottom;
+                _leftRadioButton.Checked = selectedPlacement == LegendPlacement.Left;
+                _rightRadioButton.Checked = selectedPlacement == LegendPlacement.Right;
+            }
+            finally
+            {
+                _isUpdatingUi = false;
+            }
         }
     }
 }
